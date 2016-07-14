fdb489848b8d757bcc8131d124793efda043f363
erezarny <erez.arny@gmail.com>
2016-07-14 15:40:28 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,6 @@
-"""
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
-"""
 
+from random import randrange
+n = int(randrange(1000000)) 
+print n
+if n%7==0 or n%13==0 or n%15==0:
+      print n, 'special number is'
