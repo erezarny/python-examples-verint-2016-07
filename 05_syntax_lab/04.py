95ca4d259b24907fb9bae40bba6e375549ed53ed
erezarny <erez.arny@gmail.com>
2016-07-14 15:40:21 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -1,8 +1,10 @@
-"""
-Write a program that reads lines from the user
-until an empty line is inserted.
-After the user typed in an empty line,
-print all previously inserted lines in reverse
-order (from last to first)
-"""
-
+sum="";
+while True:
+    print "enter string:"
+    string = raw_input()
+    sum += string;
+    if len(string) == 0:
+        break
+print sum
+  
+   
